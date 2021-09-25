de856edbb54ef6df18a8f9949e5e529799b64512
BrondoL <nabilunited2@gmail.com>
2021-09-25 12:48:58 +0700

    Baru home

---
+++ src/views/Foods.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="foods">
+        <Navbar />
+        <h2>Daftar Makanan</h2>
+    </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+    name: "Foods",
+    components: { Navbar },
+};
+</script>
+
+<style>
+</style>
